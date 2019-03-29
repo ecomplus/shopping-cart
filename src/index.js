84eba99f08023a8354446bcb8fe90b53066e3504
Leonardo Matos <leomp120894@gmail.com>
2019-03-29 16:53:22 -0300

    docs: settup jsdoc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+/**
+ * https://github.com/ecomclub/shopping-cart
+ * @author E-Com Club <ti@e-com.club>
+ * @license MIT
+ */
+
 'use strict'
 
-console.log(3)
+;(function () {
+  /**
+   * EcomCart object with shopping cart methods.
+   * @namespace
+   */
+
+  var EcomCart = {
+    /** EcomCart.add */
+    add: function () {}
+  }
+
+  if (typeof window === 'object' && window.document) {
+    // on browser
+    // set EcomCart object globally
+    window.EcomCart = EcomCart
+  } else if (module && module.exports) {
+    // Node.js
+    // return EcomCart object
+    module.exports = EcomCart
+  }
+}())
