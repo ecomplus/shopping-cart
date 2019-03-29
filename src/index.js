ac03b315a582038a13e1c420ff06498c37a281b0
Leonardo Matos <leomp120894@gmail.com>
2019-03-29 17:58:41 -0300

    chore: setup source structure, utils on index, separated methods

--- src/index.js
+++ src/index.js
@@ -4,26 +4,71 @@
  * @license MIT
  */
 
-'use strict'
+/**
+ * EcomCart object with shopping cart methods.
+ * @namespace
+ */
+
+var EcomCart = {}
 
 ;(function () {
-  /**
-   * EcomCart object with shopping cart methods.
-   * @namespace
-   */
+  'use strict'
 
-  var EcomCart = {
-    /** EcomCart.add */
-    add: function () {}
+  /**
+   * Current cart object.
+   * @type {object}
+   */
+  EcomCart.cart = {
+    subtotal: 0,
+    items: []
   }
 
-  if (typeof window === 'object' && window.document) {
+  /**
+   * List of cart items.
+   * @type {array.<object>}
+   */
+  EcomCart.items = EcomCart.cart.items
+
+  // use browser local storage if available
+  var db = window && window.localStorage
+  // try to load stored cart object
+  if (db) {
+    var json = db.getItem('cart')
+    try {
+      var cart = JSON.parse(json)
+      EcomCart.cart = cart
+    } catch (err) {
+      // ignore invalid cart JSON
+    }
+  }
+
+  /**
+   * Save cart object to browser localStorage.
+   */
+  EcomCart.saveCart = function () {
+    if (db) {
+      db.setItem('cart', JSON.stringify(EcomCart.cart))
+    }
+  }
+
+  /**
+   * Check item quantity, update subtotal and save new cart object.
+   * @param {object} item - Cart item object
+   */
+  EcomCart.handleItem = function (item) {
+    EcomCart.saveCart()
+  }
+
+  if (typeof window === 'object') {
     // on browser
     // set EcomCart object globally
     window.EcomCart = EcomCart
   } else if (module && module.exports) {
-    // Node.js
-    // return EcomCart object
+    /**
+     * Exports EcomCart object.
+     * @module @ecomplus/shopping-cart
+     * @returns {object} {@link EcomCart}
+     */
     module.exports = EcomCart
   }
 }())
---
+++ src/methods/add.js
@@ -0,0 +1,33 @@
+/* global EcomCart */
+
+(function () {
+  'use strict'
+
+  /**
+   * Add item to cart.
+   * @param {object} item - Cart item object
+   */
+
+  EcomCart.add = function (newItem) {
+    // check required fields
+    if (typeof newItem.product_id === 'string' &&
+      typeof newItem.quantity === 'number' && newItem.quantity >= 0 &&
+      typeof newItem.price === 'number' && newItem.price >= 0) {
+      // do the stuf
+    } else {
+      console.error(new Error('Trying to add invalid item object'))
+      return
+    }
+
+    for (var i = 0; i < EcomCart.items.length; i++) {
+      var item = EcomCart.items[i]
+      // check IDs
+      if (item.product_id === newItem.product_id && item.variation_id === newItem.variation_id) {
+        // same product and variation
+        // update quantity
+        item.quantity += newItem.quantity
+        return EcomCart.handleItem(item)
+      }
+    }
+  }
+}())
