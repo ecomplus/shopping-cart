4c388c9106f8e5fd03bc814fd2c92841a4d21410
Leonardo Matos <leomp120894@gmail.com>
2019-03-30 19:28:20 -0300

    build: fixing gulp tasks, builded and test file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { src, dest, parallel, series, watch } = require('gulp')
+const { src, dest, watch } = require('gulp')
 const browserSync = require('browser-sync').create()
 const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
@@ -12,29 +12,21 @@ const serve = () => {
     server: {
       baseDir: './tests',
       routes: {
-        '/js': './dist'
+        '/js': './src'
       }
     },
     port: 3377
   })
-  watch('./src/**/*.js', js)
   watch([
-    './src/index.js',
+    './src/**/*.js',
     './tests/index.html'
   ], browserSync.reload)
 }
 
-const js = () => {
+const dist = () => {
   return src([ './src/index.js', './src/methods/*.js' ])
     .pipe(sourcemaps.init())
     .pipe(concat('shopping-cart.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(dest('./dist'))
-}
-
-const minify = () => {
-  return src('./dist/shopping-cart.js')
-    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify())
     .pipe(rename('shopping-cart.min.js'))
     .pipe(sourcemaps.write('./'))
@@ -42,7 +34,5 @@ const minify = () => {
 }
 
 exports.serve = serve
-exports.js = js
-exports.minify = minify
-exports.dist = series(js, minify)
-exports.default = parallel(serve, js)
+exports.dist = dist
+exports.default = serve
