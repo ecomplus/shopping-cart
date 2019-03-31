8ea54318bd0f3660768876d8781e8190c3665763
Leonardo Matos <leomp120894@gmail.com>
2019-03-31 04:26:46 -0300

    feat: add 'increaseQuantity' method

---
+++ src/methods/increaseQuantity.js
@@ -0,0 +1,27 @@
+/* global EcomCart */
+
+;(function () {
+  'use strict'
+
+  /**
+   * Increase quantity of specific item by ID.
+   * @param {string} itemId - The unique object ID of item
+   * @param {integer} qnt - Quantity to increase
+   * @returns {boolean} True if the item was found
+   */
+
+  EcomCart.increaseQuantity = function (itemId, qnt) {
+    // find respective item on list by ID
+    for (var i = 0; i < EcomCart.items.length; i++) {
+      var item = EcomCart.items[i]
+      if (item._id === itemId) {
+        // item found
+        item.quantity += qnt
+        // fix new quantity if needed and save cart object
+        EcomCart.handleItem(item)
+        return true
+      }
+    }
+    return false
+  }
+}())
