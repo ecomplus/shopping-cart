cf731d0c2f4779b6bdd2cee5b9b50ff7a7fb131d
Leonardo Matos <leomp120894@gmail.com>
2019-03-31 02:16:12 -0300

    chore: handle cart reload (from storage) separated after methods ready

---
+++ src/reload.js
@@ -0,0 +1,21 @@
+/* global EcomCart */
+/* global localStorage */
+
+;(function () {
+  'use strict'
+
+  if (typeof localStorage === 'object' && localStorage) {
+    // try to load stored cart object
+    var json = localStorage.getItem('cart')
+    try {
+      var cart = JSON.parse(json)
+      if (typeof cart === 'object' && cart !== null && Array.isArray(cart.items)) {
+        cart.items.forEach(function (item) {
+          EcomCart.add(item)
+        })
+      }
+    } catch (err) {
+      // ignore invalid cart JSON
+    }
+  }
+}())
