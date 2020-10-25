6072131e00b0f1281e690187a867045694b8c6a9
Leonardo Matos <leomp120894@gmail.com>
2020-10-25 17:36:02 -0300

    fix(increase-item-qnt): do not change item final price, quantity only

--- src/methods/increase-item-qnt.js
+++ src/methods/increase-item-qnt.js
@@ -1,5 +1,4 @@
 import fixItemQuantity from './../lib/fix-item-quantity'
-import fixItemFinalPrice from './../lib/fix-item-final-price'
 import fixSubtotal from './../lib/fix-subtotal'
 
 export default ({ data, save }, emitter, [itemId, quantity = 1, canSave = true]) => {
@@ -10,7 +9,6 @@ export default ({ data, save }, emitter, [itemId, quantity = 1, canSave = true])
   item.quantity += quantity
 
   fixItemQuantity(item)
-  fixItemFinalPrice(item)
   fixSubtotal(data)
 
   /**
