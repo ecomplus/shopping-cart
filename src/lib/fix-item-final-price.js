46817e06ea37aa713a720c4d278458e65e6ecd81
Leonardo Matos <leomp120894@gmail.com>
2020-10-25 17:35:03 -0300

    fix(item-final-price): preset as item price

--- src/lib/fix-item-final-price.js
+++ src/lib/fix-item-final-price.js
@@ -1,6 +1,7 @@
 export default item => {
   // fix item final price with customizations additions
   // @TODO handle gift wrap
+  item.final_price = item.price
   if (Array.isArray(item.customizations)) {
     item.customizations.forEach(customization => {
       if (customization.add_to_price) {
